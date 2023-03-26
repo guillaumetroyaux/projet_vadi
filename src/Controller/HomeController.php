6990f7e3667a89fa6fe3ff379b85c75490d8ac1f
guillaumetroyaux <guillaumetroyaux@hotmail.fr>
2023-03-26 16:36:08 +0200

    Commencement de l'interface de la page home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class UserController extends AbstractController
+class HomeController extends AbstractController
 {
-    #[Route('/user', name: 'app_user')]
+    #[Route('/', name: 'home')]
     public function index(): Response
     {
-        return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
+        return $this->render('application/index.html.twig', [
+            'controller_name' => 'HomeController',
         ]);
     }
 }
